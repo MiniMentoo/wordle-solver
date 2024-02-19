0496d40e0780dfa7c35fa64d50a91e179c0888a4
Mostafa A <mostafaalkhazaali@gmail.com>
2024-02-19 14:08:41 +0000

    Loading answers.txt alongside the guesslist.
    Intuition behind this is that it will add those real words to the list
    twice, having my heuristic function value them above random guesses.

--- betterwordlesolver.py
+++ betterwordlesolver.py
@@ -1,12 +1,14 @@
 WORDLE_SIZE = 5
 #comment whichever one you're not using, I recommend using the full words.txt since answers.txt is outdated and might miss answers
 FILENAME = "words.txt"
-#FILENAME = "answers.txt"
+FILENAME2 = "answers.txt"
 
 from string import ascii_lowercase
 
 with open(FILENAME, "r") as words:
     lines = words.readlines()
+with open(FILENAME2, "r") as words:
+    lines += words.readlines()   
 for i in range(len(lines)):
     lines[i] = lines[i].rstrip()
 
@@ -130,6 +132,6 @@ while (loop):
     print("----Finding best guess----")
     print (findBestGuess(lines, rankList(lines)))
 
-    loop = len(lines) > 1
+    loop = len(lines) > 2
 
 print("Jobs done!")

7ab5dad3aeaea98d5d4ae8fc83d9498a6db1bee0
Mostafa A <mostafaalkhazaali@gmail.com>
2024-02-22 13:50:58 +0000

    Added a frequency table function in analyser.
    This will be used to calculate the odds of a guessed character being in
    the word, frequency is divided by the number of possible answers. It's
    possible for a words frequency to be above 1 if the average number of
    that letter in words is above 1 (ie there are a lot of possible
    duplicates).

--- analyser.py
+++ analyser.py
@@ -1,10 +1,13 @@
 from string import ascii_lowercase
+from filter import filter_by_guess
 
 class Analyser:
-    def __init__(self, words : list[str]) -> None:
-        self.words : list[str] = words.copy()
+    def __init__(self, answers : list[str], all_guesses : list[str]) -> None:
+        self.valid_words : list[str] = answers.copy()
+        self.all_guesses : list[str] = all_guesses.copy()
         self.WORDLE_SIZE : int = 5
         self.probability_list : list[dict[str, float]] = self.precompute_ranks()
+        self.frequency_list : dict[str, float] = self.precompute_frequencies()
     
     def eval_guess(self, guess : str) -> float:
         pass
@@ -16,7 +19,7 @@ class Analyser:
     
     def precompute_ranks(self) -> list[dict[str, float]]:
         rankList : list[dict[str, float]] = [] #stores 5 dictionaries, each keeping track of the probability of the letter being in that position
-        length = len(self.words)
+        length = len(self.valid_words)
         for i in range (self.WORDLE_SIZE):
             dictionary = {}
             for c in ascii_lowercase:
@@ -26,7 +29,7 @@ class Analyser:
         #for every word, add each of its letters to its ranklist in the corresponding position
         for x in range(length-1, -1, -1):
             for i in  range(self.WORDLE_SIZE):
-                c = self.words[x][i]
+                c = self.valid_words[x][i]
                 rankList[i][c] = rankList[i][c] + 1    
         
         #dividing each entry in the list with the total number to get a probability between 0 and 1
@@ -34,3 +37,21 @@ class Analyser:
             for key in rankList[i]:
                 rankList[i][key] = rankList[i][key] / length
         return rankList
+    
+    def precompute_frequencies(self) -> dict[str, float]:
+        dictionary = {}
+        length = len(self.valid_words)
+        for c in ascii_lowercase:
+            dictionary.update({c : 0})
+            
+        for word in self.valid_words:
+            for char in word:
+                dictionary[char] = dictionary[char] + 1
+        
+        for key in dictionary:
+            dictionary[key] = dictionary[key] / length
+        
+        return dictionary
+    
+    def prob_char_in_answer(self, char : str) -> float:
+        pass
--- betterwordlesolver.py
+++ betterwordlesolver.py
@@ -1,18 +1,22 @@
 WORDLE_SIZE = 5
-#comment whichever one you're not using, I recommend using the full words.txt since answers.txt is outdated and might miss answers
-FILENAME = "words.txt"
-#FILENAME = "answers.txt"
+GUESSLIST = "words.txt"
+ANSWERLIST = "answers.txt" #this is an outdated answerlist but it'll do
 
 from string import ascii_lowercase
 from analyser import Analyser
 from filter import filter_by_guess
 
-with open(FILENAME, "r") as words:
+with open(GUESSLIST, "r") as words:
     lines = words.readlines()
 for i in range(len(lines)):
     lines[i] = lines[i].rstrip()
-
 words.close()
+with open(ANSWERLIST, "r") as words:
+    valid_words = words.readlines()
+for i in range(len(valid_words)):
+    valid_words[i] = valid_words[i].rstrip()
+words.close()
+
 
 def rankList(words):
     rankList = [] #stores 5 dictionaries, each keeping track of how many times the letter appeared in that position
@@ -44,17 +48,19 @@ def findBestGuess(words, rankList):
             bestWord = word
     return bestWord
 
-loop = True
-print("----Finding best guess----")
-print (findBestGuess(lines, rankList(lines)))
-while (loop):
-    word = input("Enter a 5 letter word: ")
-    accuracy = input("How correct was the word? 0 = grey, 1 = yellow, 2 = green: ")
-    lines = filter_by_guess(word, accuracy, lines)
-    print(lines)
+if __name__ == "__main__":
+    analist = Analyser(valid_words, lines)
+    loop = True
     print("----Finding best guess----")
     print (findBestGuess(lines, rankList(lines)))
+    while (loop):
+        word = input("Enter a 5 letter word: ")
+        accuracy = input("How correct was the word? 0 = grey, 1 = yellow, 2 = green: ")
+        lines = filter_by_guess(word, accuracy, lines)
+        print(lines)
+        print("----Finding best guess----")
+        print (findBestGuess(lines, rankList(lines)))
 
-    loop = len(lines) > 1
+        loop = len(lines) > 1
 
-print("Jobs done!")
+    print("Jobs done!")
